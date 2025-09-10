8deb94dd1fb86c0d53836df806304f6b5a0e9d46
Hong, Jaeseok <ghdwotjr019@gmail.com>
2025-09-10 17:17:29 +0900

    화면 출력 방법 수정

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -4,15 +4,7 @@ public class Hello {
 
 	String message = "안녕하세요";
 
-	public Hello(){
-		this("안녕하세요");
-	}
-
-	public Hello(String message){
-		this.messsage = message;
-	}
-
-	public void say() {
-		System.out.println(message);
+	public String say(){
+		return message;
 	}
 }
